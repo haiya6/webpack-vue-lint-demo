e3b2c4324ffef47fe00c00468f241b615755cc85
xieyuhang <13451213869@163.com>
2023-02-11 21:38:08 +0800

    chore: init

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const { DefinePlugin } = require('webpack')
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+const webpackConfig = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(`src/index.ts`)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+              transpileOnly: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve('index.html')
+    }),
+    new DefinePlugin({
+      __VUE_OPTIONS_API__: `false`,
+      __VUE_PROD_DEVTOOLS__: `false`
+    })
+  ]
+}
+
+module.exports = webpackConfig
